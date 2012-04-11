45dd91d5d5dc771dc2077be53e8c437e6b85844b
Klaus Fleerkötter <klaus.fleerkoetter@zweitag.de>
2012-04-11 15:28:48 +0200

    Scale images to textwidth as a default

--- script/replace.rb
+++ script/replace.rb
@@ -2,5 +2,7 @@
 
 # Move all headlines one level up. pandoc messes that up, somehow
 contents = ARGF.read
-contents = contents.gsub('\section', '\chapter').gsub('\subsection', '\section').gsub('\subsubsection', '\subsection') 
+contents = contents.gsub('\section', '\chapter').gsub('\subsection', '\section').gsub('\subsubsection', '\subsection')
+# Set default image width to linewidth
+contents = contents.gsub('\includegraphics{', '\includegraphics[width=\linewidth]{')
 print contents
